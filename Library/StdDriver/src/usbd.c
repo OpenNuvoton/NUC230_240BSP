7090b0a6c5b5873f2a4191f582ae2f4d1fdae5ec
yjchen5 <yjchen5@nuvoton.com>
2018-07-09 16:00:32 +0800

    Fix usbd driver issue: If a ctrl in packet has the same size as the endpoint max size.
    Change-Id: I3e773af0d4e31d0c500e18d243db20c1e7713ef6

--- Library/StdDriver/src/usbd.c
+++ Library/StdDriver/src/usbd.c
@@ -48,6 +48,7 @@ static volatile uint32_t g_usbd_UsbAddr = 0;
 static volatile uint32_t g_usbd_UsbConfig = 0;
 static volatile uint32_t g_usbd_CtrlMaxPktSize = 8;
 static volatile uint32_t g_usbd_UsbAltInterface = 0;
+static volatile uint8_t  g_usbd_CtrlInZeroFlag = 0;
 
 const S_USBD_INFO_T *g_usbd_sInfo;                  /*!< A pointer for USB information structure */
 
@@ -485,6 +486,10 @@ void USBD_PrepareCtrlIn(uint8_t *pu8Buf, uint32_t u32Size)
         // Data size <= MXPLD
         g_usbd_CtrlInPointer = 0;
         g_usbd_CtrlInSize = 0;
+
+        if(u32Size == g_usbd_CtrlMaxPktSize)
+            g_usbd_CtrlInZeroFlag = 1;
+
         USBD_SET_DATA1(EP0);
         USBD_MemCopy((uint8_t *)USBD_BUF_BASE + USBD_GET_EP_BUF_ADDR(EP0), pu8Buf, u32Size);
         USBD_SET_PAYLOAD_LEN(EP0, u32Size);
@@ -503,8 +508,6 @@ void USBD_PrepareCtrlIn(uint8_t *pu8Buf, uint32_t u32Size)
   */
 void USBD_CtrlIn(void)
 {
-    static uint8_t u8ZeroFlag = 0;
-    
     DBG_PRINTF("Ctrl In Ack. residue %d\n", g_usbd_CtrlInSize);
     if(g_usbd_CtrlInSize)
     {
@@ -522,8 +525,10 @@ void USBD_CtrlIn(void)
             // Data size <= MXPLD
             USBD_MemCopy((uint8_t *)USBD_BUF_BASE + USBD_GET_EP_BUF_ADDR(EP0), (uint8_t *)g_usbd_CtrlInPointer, g_usbd_CtrlInSize);
             USBD_SET_PAYLOAD_LEN(EP0, g_usbd_CtrlInSize);
+
             if(g_usbd_CtrlInSize == g_usbd_CtrlMaxPktSize)
-                u8ZeroFlag = 1;
+                g_usbd_CtrlInZeroFlag = 1;
+
             g_usbd_CtrlInPointer = 0;
             g_usbd_CtrlInSize = 0;
         }
@@ -540,10 +545,10 @@ void USBD_CtrlIn(void)
         }
 
         /* For the case of data size is integral times maximum packet size */
-        if(u8ZeroFlag)
+        if(g_usbd_CtrlInZeroFlag)
         {
             USBD_SET_PAYLOAD_LEN(EP0, 0);
-            u8ZeroFlag = 0;
+            g_usbd_CtrlInZeroFlag = 0;
         }
         
         DBG_PRINTF("Ctrl In done.\n");
