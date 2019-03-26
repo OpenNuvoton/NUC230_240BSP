5d47a3fdc2525f6d254dd6af72021cbe05898add
phtseng <phtseng@nuvoton.com>
2019-03-26 10:45:54 +0800

    Revise to do ADC_POWER_ON() before ADC_Open() for ADC StdDriver sample codes.
    Change-Id: I150340f279fc8e61c70c9704b8a9e1e939842f60

--- SampleCode/StdDriver/ADC_ContinuousScanMode/main.c
+++ SampleCode/StdDriver/ADC_ContinuousScanMode/main.c
@@ -153,12 +153,12 @@ void AdcContScanModeTest()
         printf("  Other keys: exit continuous scan mode test\n");
         u8Option = getchar();
         if(u8Option == '1') {
+            /* Power on ADC module */
+            ADC_POWER_ON(ADC);
+
             /* Set the ADC operation mode as continuous scan, input mode as single-end and
                  enable the analog input channel 0, 1, 2 and 3 */
             ADC_Open(ADC, ADC_ADCR_DIFFEN_SINGLE_END, ADC_ADCR_ADMD_CONTINUOUS, 0xF);
-
-            /* Power on ADC module */
-            ADC_POWER_ON(ADC);
 
             /* Clear the A/D interrupt flag for safe */
             ADC_CLR_INT_FLAG(ADC, ADC_ADF_INT);
@@ -192,12 +192,12 @@ void AdcContScanModeTest()
             ADC_CLR_INT_FLAG(ADC, ADC_ADF_INT);
 
         } else if(u8Option == '2') {
+            /* Power on ADC module */
+            ADC_POWER_ON(ADC);
+
             /* Set the ADC operation mode as continuous scan, input mode as differential and
                enable analog input channel 0 and 2 */
             ADC_Open(ADC, ADC_ADCR_DIFFEN_DIFFERENTIAL, ADC_ADCR_ADMD_CONTINUOUS, 0x5);
-
-            /* Power on ADC module */
-            ADC_POWER_ON(ADC);
 
             /* Clear the A/D interrupt flag for safe */
             ADC_CLR_INT_FLAG(ADC, ADC_ADF_INT);
