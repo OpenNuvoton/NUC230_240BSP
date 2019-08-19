9aedc9504deb33dae90619a3cab6e3a169801940
yjchen5 <yjchen5@nuvoton.com>
2019-08-19 15:19:22 +0800

    1. Add USBD_MassStorage_SDCard sample code. 2. Modify typedef for USBD HID tansfer related sample code.
    Change-Id: Idf87cb407ebf7156e35daf7562be54d4218c24fe

--- SampleCode/StdDriver/USBD_HID_Transfer_and_MSC/HID_Transfer_and_MSC.c
+++ SampleCode/StdDriver/USBD_HID_Transfer_and_MSC/HID_Transfer_and_MSC.c
@@ -472,6 +472,7 @@ void HID_MSC_ClassRequest(void)
 #define SECTOR_SIZE      4096
 #define START_SECTOR     0x10
 
+#pragma pack(1)
 typedef struct {
     uint8_t u8Cmd;
     uint8_t u8Size;
@@ -479,7 +480,7 @@ typedef struct {
     uint32_t u32Arg2;
     uint32_t u32Signature;
     uint32_t u32Checksum;
-} __attribute__((packed)) CMD_T;
+} CMD_T;
 
 CMD_T gCmd;    
 
--- SampleCode/StdDriver/USBD_Printer_and_HID_Transfer/micro_printer_and_hid_transfer.c
+++ SampleCode/StdDriver/USBD_Printer_and_HID_Transfer/micro_printer_and_hid_transfer.c
@@ -288,6 +288,7 @@ void PTR_Data_Receive(void)
 #define SECTOR_SIZE      4096
 #define START_SECTOR     0x10
 
+#pragma pack(1)
 typedef struct {
     uint8_t u8Cmd;
     uint8_t u8Size;
@@ -295,7 +296,7 @@ typedef struct {
     uint32_t u32Arg2;
     uint32_t u32Signature;
     uint32_t u32Checksum;
-} __attribute__((packed)) CMD_T;
+} CMD_T;
 
 CMD_T gCmd;    
 
