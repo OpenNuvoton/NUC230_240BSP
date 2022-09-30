7e5e6dc17281c7a4c294bdd89d9ff5fa5c4010ac
MS20 YJChen5 <YJCHEN5@nuvoton.com>
2022-09-30 10:48:27 +0800

    USBD VCOM sample code: Call UART FIFO size from uart.h.
    Change-Id: I60975791523b9357fa7a190dc32959ad0d2be12b

--- SampleCode/StdDriver/USBD_VCOM_DualPort/main.c
+++ SampleCode/StdDriver/USBD_VCOM_DualPort/main.c
@@ -23,9 +23,6 @@ uint16_t gCtrlSignal1 = 0;     /* BIT0: DTR(Data Terminal Ready) , BIT1: RTS(Req
 #define RXBUFSIZE           512 /* RX buffer size */
 #define TXBUFSIZE           512 /* RX buffer size */
 
-#define TX_FIFO_SIZE_0      64  /* TX Hardware FIFO size */
-#define TX_FIFO_SIZE_1      64  /* TX Hardware FIFO size */
-
 /*---------------------------------------------------------------------------------------------------------*/
 /* Global variables                                                                                        */
 /*---------------------------------------------------------------------------------------------------------*/
@@ -213,9 +210,9 @@ void UART02_IRQHandler(void)
         {
             /* Fill the Tx FIFO */
             size = comTbytes0;
-            if(size >= TX_FIFO_SIZE_0)
+            if(size >= UART0_FIFO_SIZE)
             {
-                size = TX_FIFO_SIZE_0;
+                size = UART0_FIFO_SIZE;
             }
 
             while(size)
@@ -278,9 +275,9 @@ void UART1_IRQHandler(void)
         {
             /* Fill the Tx FIFO */
             size = comTbytes1;
-            if(size >= TX_FIFO_SIZE_1)
+            if(size >= UART1_FIFO_SIZE)
             {
-                size = TX_FIFO_SIZE_1;
+                size = UART1_FIFO_SIZE;
             }
 
             while(size)
